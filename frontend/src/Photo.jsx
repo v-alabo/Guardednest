a66c5d68d1e1a211ae0cd36be98a9a6879119819
Wayward <victoralabo99@gmail.com>
2024-08-19 22:40:05 +0100

    first

--- frontend/src/Photo.jsx
+++ frontend/src/Photo.jsx
@@ -81,6 +81,7 @@ export default function Settings() {
                     <ion-icon name="home-outline"></ion-icon>
                   </span>
                   <span className="title ">Dashboard</span>
+                  <p>ju</p>
                 </Link>
               </li>
               <li>
